8fa729dff6fd2d279367229ba7984ba95f95823c
cloudy24dotcom <cloudy24.com@gmail.com>
2022-01-20 16:13:59 +0530

    404 Page added

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
-import DesktopMenu from "./DesktopMenu"
+import DesktopMenu from "./DesktopMenu";
 import MobileMenu from "./MobileMenu";
+import Logo from "./Logo";
 function Header() {
   return (
     <>
+      <Logo />
       <h1>Header</h1>
       <DesktopMenu />
       <br />
---
+++ pages/404.jsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Link from "next/link";
+const NotFound = () => {
+  return (
+    <>
+      <Head>
+        <title>404 - Company </title>
+        <meta name="description" content="content description" />
+        <meta name="keywords" content="key1, key2, key3" />
+      </Head>
+      <center>Page Not Found My Page || 404</center>
+    </>
+  );
+};
+
+export default NotFound;
